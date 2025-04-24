9542157f9d70c9a8bc5bf4e812846bc99ab068c5
Samuel Gauthier <samuel.gauthier@zuehlke.com>
2025-04-24 18:38:15 +0200

    fix formatting

--- src/examples/save_data_to_disk/main.cpp
+++ src/examples/save_data_to_disk/main.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
 #include <fstream>
-#include <vector>
-#include <string>
+#include <iostream>
 #include <nlohmann/json.hpp>
+#include <string>
+#include <vector>
 
 using json = nlohmann::json;
 using namespace std;
@@ -13,25 +13,26 @@ using namespace std;
  * @param json_data The JSON string containing acquisition data.
  * @param csv_filename The name of the CSV file to save the data to.
  */
-void save_acquisition_data_to_csv(const string& json_data, const string& csv_filename) {
-    json data = json::parse(json_data);
+void save_acquisition_data_to_csv(const string& json_data,
+                                  const string& csv_filename) {
+  json data = json::parse(json_data);
 
-    const auto& x_data = data["acquisition"][0]["roi"][0]["xData"];
-    const auto& y_data = data["acquisition"][0]["roi"][0]["yData"][0];
+  const auto& x_data = data["acquisition"][0]["roi"][0]["xData"];
+  const auto& y_data = data["acquisition"][0]["roi"][0]["yData"][0];
 
-    ofstream file(csv_filename);
-    if (!file.is_open()) {
-        cerr << "Error opening file!" << endl;
-        return;
-    }
+  ofstream file(csv_filename);
+  if (!file.is_open()) {
+    cerr << "Error opening file!" << endl;
+    return;
+  }
 
-    file << "xData,yData\n";
+  file << "xData,yData\n";
 
-    for (size_t i = 0; i < x_data.size(); ++i) {
-        file << x_data[i] << "," << y_data[i] << "\n";
-    }
+  for (size_t i = 0; i < x_data.size(); ++i) {
+    file << x_data[i] << "," << y_data[i] << "\n";
+  }
 
-    file.close();
+  file.close();
 }
 
 /**
@@ -40,44 +41,54 @@ void save_acquisition_data_to_csv(const string& json_data, const string& csv_fil
  * @param json_data The JSON data containing SpectrAcq3 data.
  * @param csv_filename The name of the CSV file to save the data to.
  */
-void save_spectracq3_data_to_csv(const vector<json>& json_data, const string& csv_filename) {
-    vector<string> headers = {
-        "wavelength", "elapsedTime", "currentSignal_value", "currentSignal_unit", "pmtSignal_value", "pmtSignal_unit",
-        "ppdSignal_value", "ppdSignal_unit", "voltageSignal_value", "voltageSignal_unit",
-        "eventMarker", "overscaleCurrentChannel", "overscaleVoltageChannel", "pointNumber"
-    };
+void save_spectracq3_data_to_csv(const vector<json>& json_data,
+                                 const string& csv_filename) {
+  vector<string> headers = {"wavelength",
+                            "elapsedTime",
+                            "currentSignal_value",
+                            "currentSignal_unit",
+                            "pmtSignal_value",
+                            "pmtSignal_unit",
+                            "ppdSignal_value",
+                            "ppdSignal_unit",
+                            "voltageSignal_value",
+                            "voltageSignal_unit",
+                            "eventMarker",
+                            "overscaleCurrentChannel",
+                            "overscaleVoltageChannel",
+                            "pointNumber"};
 
-    ofstream file(csv_filename);
-    if (!file.is_open()) {
-        cerr << "Error opening file!" << endl;
-        return;
-    }
+  ofstream file(csv_filename);
+  if (!file.is_open()) {
+    cerr << "Error opening file!" << endl;
+    return;
+  }
 
-    for (const auto& header : headers) {
-        file << header << ",";
-    }
-    // Remove last comma
-    file.seekp(-1, ios_base::end);
-    file << "\n";
+  for (const auto& header : headers) {
+    file << header << ",";
+  }
+  // Remove last comma
+  file.seekp(-1, ios_base::end);
+  file << "\n";
 
-    for (const auto& data : json_data) {
-        file << data["wavelength"] << ","
-             << data["elapsedTime"] << ","
-             << data["currentSignal"]["value"] << "," << data["currentSignal"]["unit"] << ","
-             << data["pmtSignal"]["value"] << "," << data["pmtSignal"]["unit"] << ","
-             << data["ppdSignal"]["value"] << "," << data["ppdSignal"]["unit"] << ","
-             << data["voltageSignal"]["value"] << "," << data["voltageSignal"]["unit"] << ","
-             << data["eventMarker"] << ","
-             << data["overscaleCurrentChannel"] << ","
-             << data["overscaleVoltageChannel"] << ","
-             << data["pointNumber"] << "\n";
-    }
+  for (const auto& data : json_data) {
+    file << data["wavelength"] << "," << data["elapsedTime"] << ","
+         << data["currentSignal"]["value"] << ","
+         << data["currentSignal"]["unit"] << "," << data["pmtSignal"]["value"]
+         << "," << data["pmtSignal"]["unit"] << ","
+         << data["ppdSignal"]["value"] << "," << data["ppdSignal"]["unit"]
+         << "," << data["voltageSignal"]["value"] << ","
+         << data["voltageSignal"]["unit"] << "," << data["eventMarker"] << ","
+         << data["overscaleCurrentChannel"] << ","
+         << data["overscaleVoltageChannel"] << "," << data["pointNumber"]
+         << "\n";
+  }
 
-    file.close();
+  file.close();
 }
 
 int main() {
-    string json_data = R"({
+  string json_data = R"({
         "acquisition": [{
             "acqIndex": 1,
             "roi": [{
@@ -95,24 +106,22 @@ int main() {
         "timestamp": "2025.03.24 10:04:51.838"
     })";
 
-    save_acquisition_data_to_csv(json_data, "acquisition_data.csv");
+  save_acquisition_data_to_csv(json_data, "acquisition_data.csv");
 
-    vector<json> spectracq3_data = {
-        {
-            {"wavelength", 650},
-            {"elapsedTime", 1.23},
-            {"currentSignal", {{"value", 0.5}, {"unit", "mA"}}},
-            {"pmtSignal", {{"value", 1.1}, {"unit", "V"}}},
-            {"ppdSignal", {{"value", 2.0}, {"unit", "nA"}}},
-            {"voltageSignal", {{"value", 0.75}, {"unit", "V"}}},
-            {"eventMarker", "A"},
-            {"overscaleCurrentChannel", false},
-            {"overscaleVoltageChannel", false},
-            {"pointNumber", 1}
-        },
-    };
+  vector<json> spectracq3_data = {
+      {{"wavelength", 650},
+       {"elapsedTime", 1.23},
+       {"currentSignal", {{"value", 0.5}, {"unit", "mA"}}},
+       {"pmtSignal", {{"value", 1.1}, {"unit", "V"}}},
+       {"ppdSignal", {{"value", 2.0}, {"unit", "nA"}}},
+       {"voltageSignal", {{"value", 0.75}, {"unit", "V"}}},
+       {"eventMarker", "A"},
+       {"overscaleCurrentChannel", false},
+       {"overscaleVoltageChannel", false},
+       {"pointNumber", 1}},
+  };
 
-    save_spectracq3_data_to_csv(spectracq3_data, "spectracq3_data.csv");
+  save_spectracq3_data_to_csv(spectracq3_data, "spectracq3_data.csv");
 
-    return 0;
+  return 0;
 }
